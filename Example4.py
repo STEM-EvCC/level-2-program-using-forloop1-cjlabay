98b91d3ee5bef917f6c287e130f0c18d97465667
Casey LaBay <cjlabay@students.everettcc.edu>
2025-05-19 20:43:05 +0000

    Make updated

--- Example4.py
+++ Example4.py
@@ -2,5 +2,6 @@
 # prints a multiplication table from 1 to 5
 for i in range(1, 6):
     for j in range(1, 6):
-        print(f"{i} x {j} = {i * j}")
+        if i == j:
+            print(f"{i} x {j} = {i * j}")
     print()  # Blank line for better readability
--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+# Counts the total number of space missions.
+# Counts the number of successful missions.
+# Calculates the success rate of the missions.
+# Lists all the missions that were launched before the year 2000.
+
 mission_names = ['Apollo 11', 'Challenger', 'Curiosity Rover', 'Viking 1', 'Mars Pathfinder', 'Hubble Telescope', 'Apollo 13']
 mission_years = [1969, 1986, 2012, 1975, 1996, 1990, 1970]
 mission_success = [True, False, True, True, True, True, False]
+
+print(f"Total number of missions: {len(mission_names)}")
+print(f"Number of successful missions: {mission_success.count(True)}")
+print(f"Success rate: {"{:.2f}".format((mission_success.count(True) / len(mission_success)) * 100)}%")
+print("Missions launched before the year 2000:")
+missions_before_2000 = []
+for i in range(len(mission_names)):
+    if mission_years[i] <2000:
+        missions_before_2000.append(mission_names[i])
+for mission in missions_before_2000:
+    print("- " + mission)
